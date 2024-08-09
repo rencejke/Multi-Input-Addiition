1446098d0c4c880522cff7cb6c813aa9b6334204
rence <99105241+rencejke@users.noreply.github.com>
2024-08-09 23:37:49 +0800

    added validation

--- script.js
+++ script.js
@@ -4,15 +4,30 @@ const total = document.querySelector(`.totalAmount`);
 
 
 
+
+
 submitButton.addEventListener('click', (e) =>{
 
     let sum = 0;
-    for(let i = 1; i < prices.length; i++)
+    for(let i = 0; i < prices.length; i++)
         {
-            //number() -  convert  string to number
-            sum += Number(prices[i].value)
+            //check if all input fields are filled 
+            if(prices[i].value == "" ||  prices[i].value == null)
+            {
+               alert("Plese filled all the input field")
+               return
+            }
+            else
+            {
+                    //number() -  convert  string to number
+                    sum += Number(prices[i].value)
+            }
+
+        
         }
 
     total.value = sum;
+
+
 })
 
