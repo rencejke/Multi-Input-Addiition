22b31cffaa29eb7b33a8c0b5e422dcfa361a4719
rence <99105241+rencejke@users.noreply.github.com>
2024-08-09 22:28:11 +0800

    refresher

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ const total = document.querySelector(`.totalAmount`);
 submitButton.addEventListener('click', (e) =>{
 
     let sum = 0;
-
-    for(let i = 1; i  < prices.length; i++)
+    for(let i = 1; i < prices.length; i++)
         {
+            //number() -  convert  string to number
             sum += Number(prices[i].value)
         }
 
